80388d13e9ad05b1d9ff8a97513f0a447623881b
Ian Goodrich <44086921+specialprocedures@users.noreply.github.com>
2020-08-24 19:14:59 +0400

    now works properly, I hope

--- random_order/make_random.py
+++ random_order/make_random.py
@@ -9,7 +9,7 @@ import pandas as pd
 df = pd.read_csv('q2_sourcelab.csv')
 
 # Set the number of permutations
-num_perms = 1000
+num_perms = 30
 
 # Check groups of labels are evenly sized, important for multilanguage
 lab_groups = df['col'].unique()
@@ -31,21 +31,23 @@ out = {}
 for group in lab_groups:
     out[group] = []
     labs = df[df['col'] == group]['lab'].values
-    
+    measure = df[df['col'] == group]['measurement'].values
     for idx in indices:
-        out[group].append(labs[list(idx)])
+        out[group].append((labs[list(idx)], measure[list(idx)]))
 
 table = {}
 for group, vals in out.items():
     table[group] = []
 
     table['condition'] = []
+    table['measurement'] = []
     for n, i in enumerate(vals):
-        for m, x in enumerate(i):
-            table[group].append(x)
+        for m, x in enumerate(zip(i[0], i[1])):
+            table[group].append(x[0])
+            table['measurement'].append(x[1])
             table['condition'].append(f"{n+1}-V{m+1}")
 
-final = pd.DataFrame(table)[['condition'] + list(lab_groups)]
+final = pd.DataFrame(table)[['condition', 'measurement'] + list(lab_groups)]
 final.to_csv('q2.csv')
 # %%
 
